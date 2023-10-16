cdce5b9195234cfd54dcebf1f6e5d52138a208ea
GusMonMo <gustavo.mo.monteiro@gmail.com>
2023-10-16 13:15:28 -0400

    Start

---
+++ src/LandingPage.jsx
@@ -0,0 +1,76 @@
+
+function App() {
+  return (
+    <>
+      <div className='cabecalho'>
+        <div className='logo1'/>
+      </div>
+      <div className='capa'>
+        <div className="gradient1">
+          <h1>Resolva suas questões trabalhistas agora</h1>
+          <h2>O escritório mais eficiente e melhor avaliado</h2>
+          <div className="button" style={{width:'20vw', height:'2vw', marginLeft:'60vw', marginTop:'3vw'}}>Falar com um especialista</div>
+        </div>
+      </div>
+
+      <div className='informacoes'>
+        <div className="infoBox">
+          <h2>Fale com quem melhor entende do assunto</h2>
+          <p>Mais de 999 anos de experiência no mercado</p>
+          <p>Atuando em mais de 999 processos judiciais</p>
+          <p>Agilidade na prestação de serviço, honestidade e excelência</p>
+        </div>
+        <div className="imagem">
+          <div className="img"/>
+        </div>
+      </div>
+
+      <div className='apresentacao'>
+        <div className="apreBox">
+          <h2>Dr. Gabriel Smith, advogado trabalhista</h2>
+          <h3>Sócio Fundador - OAB/SP No 123.456</h3>
+          <p>Advogado trabalhista graduado em Direito e pós-graduado em direito trabalhista pela Harvard University.</p>
+          <div className="second button">Conheça nosso time</div>
+          <div className="button">Entre em contato</div>
+        </div>
+        <div className="img2"/>
+      </div>
+      <div className='servicos'>
+      
+        <div className="midPageLogo"/>
+        
+        <h2>Nossas Especialidades</h2>
+        <div className="servBox">
+          <h3>Trabalho sem carteira assinada</h3>
+          <p>Está trabalhando em todas as condições de um CLT? Você pode solicitar o reconhecimento do vínculo empregatício.</p>
+        </div>
+        <div className="servBox" style={{marginLeft:'53vw'}}>
+        <h3>Reversão de Justa Causa</h3>
+        <p>A empresa não provou que você cometeu uma falta grave? Essa situação pode ser revertida e você pode ter todos os direitos de um trabalhador demitido sem justa causa.</p>
+        </div>
+        <div className="servBox">
+          <h3>Horas Extras</h3>
+          <p>Trabalha além do seu turno normal? Saiba que você tem direito a ser compensado por essas horas e por todas as taxas correspondentes.</p>
+        </div>
+      </div>
+
+      <div className='fim'>
+        <h2>Exija o cumprimento dos seus direitos garantidos por lei</h2>
+        <div className="button">Quero falar com um especialista</div>
+      </div>
+
+      <div className='rodape'>
+        <div className="img3"/>
+        <div className="iconMail"/>
+        <div className="iconZap"/>
+        <h3>contato@smithadvocacia.com</h3>
+        <h3>(00)12345-6790</h3>
+          <div className="aviso">
+            <h2>Aviso de página fictícia </h2>
+            <p>Esta página e todos os dados contidos nela são fictícios, possui como único propósito demonstração de trabalho e habilidades, nenhum dado ou informação deve ser tomado como real.</p>
+          </div>
+      </div>
+    </>
+  );
+}
+export default App;
